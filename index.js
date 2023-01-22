0ad85ba0fc16420c4d167d3ed0ff4260410aeba8
TamzinC <115940593+TamzinC@users.noreply.github.com>
2023-01-22 22:20:33 +0000

    progress code. able to generate a basic readme file. further tweaks needed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
@@ -7,33 +6,77 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
     {
         name: 'projectTitle',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        type: 'input'
     },
     {
         name: 'description',
-        message: 'Tell me about your app'
+        message: 'Tell me about your app:',
+        type: 'input'
+    },
+    {
+        name: 'install',
+        message:'Does your app require installation? If so, please provide step-by-step instructions:',
+        type: 'input'
+    },
+    {
+        name: 'usage',
+        message: 'What are the instuctions to use the app?',
+        type: 'input'
+    },
+    {
+        name: 'license',
+        message: 'What licence do you require?',
+        type: 'input'
+    },
+    {
+        name: 'contribute',
+        message: 'Please explain any guidelines for other devlopers if you would like them to contribute towards your project:',
+        type: 'input'
+    },
+    {
+        name: 'tests',
+        message: 'Are there any tests for your app? If so, please add them here:',
+        type: 'input'
+    },
+    {
+        name: 'githubID',
+        message: 'Please enter your GitHub username:',
+        type: 'input'
+    },
+    {
+        name: 'githubURL',
+        message: 'Please provide the URL to your GitHub profile:',
+        type: 'input'
+    },
+    {
+        name: 'email',
+        message: 'Please enter your email address:',
+        type: 'input'
     }
 ];
 
 
 // function to write README file
-function writeToFile(fileName, data) {
-    return fs.writeFile(questions.projectTitle + 'md', questions.description, (error))
-    .then(() => {
+function writeToFile(projectTitle, data) {
+    return fs.writeFile('README.md', data, (error) => {
         if (error) {
             return console.log(error);
         } else{
-            console.log(`You have successfully created the ${data.projectTitle} README file`);
-            console.log(`The description of your app is:\n${data.description}`);
+            console.log(`-------You have successfully created your README file for ${projectTitle}!-------`);
         }
     });
 };
 
 // function to initialize program
 function init() {
-    inquirer.prompt([questions])
-    writeToFile();
-    generateMarkdown();
+    console.log('-------Welcome to the README Generator!-------');
+    inquirer.prompt(questions).then((answers) => {
+        console.log({answers});
+        const markdown = generateMarkdown(answers);
+        console.log({markdown});
+        writeToFile(answers.projectTitle, markdown);
+    })
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.projectTitle}
 
+---
+
+## Description
+ ${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+## Licence
+${data.license}
+
+## Contributing
+${data.contribute}
+
+## Tests
+${data.tests}
+
+## Questions
+Visit my profile: [${data.githubID}](${data.githubURL})
+
+If you have any questions or feedback for this app, please click [here](${data.email}) to contact me.
 `;
 }
 
